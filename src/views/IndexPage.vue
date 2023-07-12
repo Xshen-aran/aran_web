fe84a6033f529f3ddb8758658d2a039545bb086b
王楠翔 <wnx991@163.com>
2023-07-12 18:10:53 +0800

    init package

---
+++ src/views/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <a-layout>
+        <a-layout-sider>Sider</a-layout-sider>
+        <a-layout>
+            <a-layout-header>Header</a-layout-header>
+            <a-layout-content>Content</a-layout-content>
+            <a-layout-footer>Footer</a-layout-footer>
+        </a-layout>
+    </a-layout>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue';
+const collapsed = ref<boolean>(false);
+const selectedKeys = ref<string[]>(['1']);
+</script>
+
+<style scoped>
+#components-layout-demo-basic .ant-layout-sider {
+    color: #fff;
+    line-height: 120px;
+    background: #3ba0e9;
+}
+
+[data-theme='dark'] #components-layout-demo-basic .ant-layout-sider {
+    background: #3499ec;
+}
+
+#components-layout-demo-basic .code-box-demo {
+    text-align: center;
+}
+
+#components-layout-demo-basic .ant-layout-header,
+#components-layout-demo-basic .ant-layout-footer {
+    color: #fff;
+    background: #7dbcea;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import process from 'vite-plugin-progress'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), process()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url))
     }
   }
 })
